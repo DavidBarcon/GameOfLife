67353b17f434d047f5418f66147beebcae689656
David Barcón <dbarcon@domingoalonsogroup.com>
2023-10-13 11:39:33 +0100

    created new classes. Board and cell

---
+++ GameOfLife/Board.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    internal class Board
+    {
+
+        List<Cell> board;
+
+        //initialize the board with a 2d array
+        public Board(bool[,] bools) {
+            initialize(bools);
+        }
+
+        //when called empty creates a 10x10 board with all cells dead
+        public Board()
+        {
+            initialize(new bool[10,10]);
+        }
+
+
+        public void initialize(bool[,] bools)
+        {
+            board = new List<Cell>();
+
+            for (int x = 0; x < bools.GetLength(0); x++)
+            {
+                for (int y = 0; y < bools.GetLength(1); y++)
+                {
+                    board.Add(new Cell(bools[x, y], x, y));
+                }
+            }
+        }
+
+        private Cell findCell(int x, int y)
+        {
+            return board.Find(cell =>
+                cell.x == x && cell.y == y
+            );
+        }
+
+    }
+}
---
+++ GameOfLife/cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    internal class Cell
+    {
+        public bool isAlive { get; }
+        public int x { get; }
+        public int y { get; }
+
+        public Cell(bool isAlive, int x, int y ) { 
+            this.isAlive = isAlive;
+            this.x = x;
+            this.y = y; 
+        }   
+
+    }
+}
